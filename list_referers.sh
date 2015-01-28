b4cb300301f469aaf0ff059b408118aa65a13731
Alex Becker <alcubecker@gmail.com>
2015-01-28 00:00:01 -0600

    referers working, user_agents bugged

---
+++ list_referers.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/gawk -f
+# list the referers in an apache log (with default logging) and how many times each appears
+
+{
+	count[$11]++;
+}
+
+END {
+	len = 0;
+	for (c in count) {
+		len++;
+	}
+
+	for (i = 0; i < len; i++) {
+		min = 999999999;
+		for (c in count) {
+			if (count[c] < min) {
+				min = count[c];
+			}
+		}
+		for (c in count) {
+			if (count[c] == min) {
+				if (c != "\"-\"") {
+					print min, c;
+				}
+				delete count[c];
+			}
+		}
+	}
+}
